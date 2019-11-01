989ecda31db61664fc810e058e11520ff32feb2c
PinchukVitaliy@ukr.net <PinchukVitaliy@ukr.net>
2019-11-01 15:47:19 +0200

    deploy heroku: update 3

--- src/main/java/pink/coursework/csvparser/security/SecurityConfiguration.java
+++ src/main/java/pink/coursework/csvparser/security/SecurityConfiguration.java
@@ -17,11 +17,6 @@ import pink.coursework.csvparser.config.AuthSuccessApplicationListener;
 import pink.coursework.csvparser.config.CustomLogoutHandler;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  * настройка конфигураций spring security
@@ -134,13 +129,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
 }
