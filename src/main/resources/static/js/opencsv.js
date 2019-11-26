4237e7bd9b96e396f3b17af94cdb156b36809316
PinchukVitaliy@ukr.net <PinchukVitaliy@ukr.net>
2019-11-26 17:35:25 +0200

    rename att name in modals

--- src/main/resources/static/js/opencsv.js
+++ src/main/resources/static/js/opencsv.js
@@ -3,12 +3,15 @@
      function openModalSave() {
                       $("#openModalSave").modal();
                   }
+
     function openModalNewColum() {
              $("#openModalNewColum").modal();
          }
+
     function openModalNewRow() {
                  $("#openModalNewRow").modal();
              }
+
     function delRows(){
         var x = document.getElementById("delrows");
         var parent = document.getElementById("parentDiv");
